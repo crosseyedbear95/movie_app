dd64e42bc09037af79be2a8b20b19d87517693f8
crosseyedbear95 <crosseyedbear95@gmail.com>
2020-05-30 01:36:50 +0900

    Updates

---
+++ precache-manifest.552a8196fafdbd11bcda4da0b80c9fbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695304166bc183c14fd56a3089d789c4",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "d886a685285a1589a410",
+    "url": "/movie_app/static/css/main.6c11559f.chunk.css"
+  },
+  {
+    "revision": "78a8c6d4641937053d35",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d886a685285a1589a410",
+    "url": "/movie_app/static/js/main.fe7d2436.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
